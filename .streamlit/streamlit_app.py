f2f3a797f8522c1e898b2604f2c33ca960310508
emiilyzhao <emilyjjz@gmail.com>
2024-07-29 16:14:13 -0400

    Update streamlit_app.py

--- .streamlit/streamlit_app.py
+++ .streamlit/streamlit_app.py
@@ -6,3 +6,13 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+path_to_html = "/Users/emilyzhao/li-sound/mapping/bottom-do.html" 
+
+# Read file and keep in variable
+with open(path_to_html,'r') as f: 
+    html_data = f.read()
+
+## Show in webpage
+st.header("Show an external HTML")
+st.components.v1.html(html_data,height=200)
