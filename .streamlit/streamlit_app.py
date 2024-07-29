3128ce9d240c298af14f5fd39997420000bcb04f
emiilyzhao <emilyjjz@gmail.com>
2024-07-29 16:10:32 -0400

    Update streamlit_app.py

--- .streamlit/streamlit_app.py
+++ .streamlit/streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import streamlit.components.v1 as components
+
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+graph = open('/Users/emilyzhao/li-sound/mapping/surface-do.html')
+components.html(graph.read())
