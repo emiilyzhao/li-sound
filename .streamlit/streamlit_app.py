a2b27f2b5e18701ae68713ca65de3972cafd9ae3
emiilyzhao <emilyjjz@gmail.com>
2024-07-29 16:12:09 -0400

    Update streamlit_app.py

--- .streamlit/streamlit_app.py
+++ .streamlit/streamlit_app.py
@@ -9,3 +9,11 @@ st.write(
 
 #graph = open('/Users/emilyzhao/li-sound/mapping/surface-do.html')
 #components.html(graph.read())
+
+plot_file = open('/Users/emilyzhao/li-sound/mapping/surface-do.html','r')
+
+plot = plot_file.read()
+
+st.markdown(plot,unsafe_allow_html=True)
+
+plot = plot_file.close()
