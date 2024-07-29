47587f4bb8d2c6fbf7d5e81a3928151e1b73928a
emiilyzhao <emilyjjz@gmail.com>
2024-07-29 16:11:06 -0400

    Update streamlit_app.py

--- .streamlit/streamlit_app.py
+++ .streamlit/streamlit_app.py
@@ -7,5 +7,5 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-graph = open('/Users/emilyzhao/li-sound/mapping/surface-do.html')
-components.html(graph.read())
+#graph = open('/Users/emilyzhao/li-sound/mapping/surface-do.html')
+#components.html(graph.read())
