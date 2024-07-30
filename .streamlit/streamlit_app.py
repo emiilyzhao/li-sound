e491c063cc521bf02a928da791f293f02a733d88
emiilyzhao <emilyjjz@gmail.com>
2024-07-30 11:58:16 -0400

    what

--- .streamlit/streamlit_app.py
+++ .streamlit/streamlit_app.py
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-import streamlit as st
-import streamlit.components.v1 as components
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-
-graph = open('/Users/emilyzhao/li-sound/mapping/surface-do.html')
-components.html(graph.read())
-
-=======
 import numpy as np
 import pandas as pd
+import folium 
+import geopandas as gpd
 import streamlit as st
+import streamlit.components.v1 as components
+from streamlit_folium import folium_static
 from final_functions import mapping, time_series
 
 st.title("🎈 My new app")
@@ -25,4 +16,3 @@ surface_data = pd.read_csv('data/surface_water_quality.csv')
 surface_time = folium_static(mapping(surface_data))
 
 st.write("## Time Series")
->>>>>>> 22d3fcc0f8df05ac8c62705b3808c88cc1464f9a
